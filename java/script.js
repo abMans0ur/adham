64cb73948250ffe236ed226e1ff79df7ed012937
abMans0ur <abmansour707@gmail.com>
2024-07-22 14:55:10 +0300

    first commit

---
+++ java/script.js
@@ -0,0 +1,48 @@
+var name="adham";
+var int=20;
+var float=20000.55;
+var gg=null;
+var og=undefined;
+var array=["mazen","adham","ramy"];
+var names= {
+    id:202202647,
+    managerid:null,
+    age:22
+}
+console.log(name);
+console.log(int);
+console.log(float);
+console.log(array);
+console.log(names);
+console.log(gg);
+console.log(og);
+console.log(typeof(name));
+console.log(typeof(int));
+console.log(typeof(float));
+console.log(typeof(array));
+console.log(typeof(names));
+console.log(typeof(gg));
+console.log(typeof(og));
+console.log("youssif");
+console.log(42);
+console.log(2595.44);
+var x=60;
+var y=40;
+var z=x+y;
+var a=x-y;
+var b=x*y
+var c=x/y;
+var d=x%y;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(b);
+console.log(d);
+if(x==60)
+{
+    console.log("hello");
+}else if(x!=60){
+    console.log("hi");
+}
